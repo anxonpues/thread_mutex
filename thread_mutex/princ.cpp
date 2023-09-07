2429262e5d4f43d53ae485dc2a335fd5e3baa5a5
Anxon Pues <43876773+anxonpues@users.noreply.github.com>
2023-09-07 09:22:27 +0200

    add option2 using lock_guard instead of unique_lock

--- thread_mutex/princ.cpp
+++ thread_mutex/princ.cpp
@@ -18,6 +18,18 @@ void driveCar(string driverName) {
 	carLock.unlock();
 }
 
+void driveCar2(string driverName) {
+	lock_guard<mutex> carLock(carMutex);
+	// the following 3 lines are critical job
+	// need to stay locked when executed to prevent
+	// simultaneous executing with mutex
+	cout << driverName << " is driving " << endl;
+	this_thread::sleep_for(chrono::seconds(2));
+	cout << driverName << " is done driving " << endl;
+	// after the end of critical job  do unlock
+	
+}
+
 int main()
 {
 	thread t1(driveCar, "Saldina");
@@ -25,6 +37,12 @@ int main()
 
 	t1.join();
 	t2.join();
+
+	thread t3(driveCar2, "Saldina 2");
+	thread t4(driveCar2, "Eithan 2");
+
+	t3.join();
+	t4.join();
 
 
 	cin.get();
